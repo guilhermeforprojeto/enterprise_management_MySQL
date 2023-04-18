fb2de70de853233151646d8b0cc170a4ef26c4b4
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-04-17 22:43:25 -0300

    feat: model para Departamento

--- django/GerenciamentoEmpresarial/empresa/models.py
+++ django/GerenciamentoEmpresarial/empresa/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+
+class Departamento(models.Model):
+    ID_Departamento = models.CharField(
+        primary_key=True, max_length=4, editable=False)
+    Nome_Departamento = models.CharField(max_length=40)
+
+    def save(self, *args, **kwargs):
+        if not self.ID_Departamento:
+            # Gera um novo UUID e guarda apenas os 4 primeiros caracteres
+            self.ID_Departamento = uuid.uuid4().hex[:4]
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'Departamento'
