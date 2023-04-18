acc66ab6908d3cfb260dcba59c0ee464ac7eb7e2
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-04-17 22:45:19 -0300

    feact: confg do para banco mysql e install apps para a app empresa mais + o rest_framework

--- django/GerenciamentoEmpresarial/GerenciamentoEmpresarial/settings.py
+++ django/GerenciamentoEmpresarial/GerenciamentoEmpresarial/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'empresa',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,17 @@ WSGI_APPLICATION = 'GerenciamentoEmpresarial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Empresa',
+        'USER': 'guilhermepython',
+        'PASSWORD': 'a.123456',
+        'HOST': '127.0.0.1',
+        'PORT': '2023',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'autocommit': True,
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
