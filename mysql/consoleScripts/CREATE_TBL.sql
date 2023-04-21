1b6d2309d2a4442d69722a37f69bc38e875cdfa9
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-04-20 21:43:16 -0300

    chore:  requisitos Anexo1 Cada funcionário  possui Nome, CPF e RG únicos

--- mysql/consoleScripts/CREATE_TBL.sql
+++ mysql/consoleScripts/CREATE_TBL.sql
@@ -3,17 +3,16 @@
 ###########################################################
 
 
-CREATE TABLE departamento (
+CREATE TABLE Departamento (
   ID_Departamento INT(4) PRIMARY KEY,
   Nome_Departamento VARCHAR(40) UNIQUE
 );
 
-
 CREATE TABLE Funcionario (
-  ID_Funcionario INT(4) PRIMARY KEY,
-  Nome_Funcionario VARCHAR(40),
-  CPF_Funcionario VARCHAR(11),
-  RG_Funcionario VARCHAR(9),
+  ID_Funcionario INT(4) PRIMARY KEY AUTO_INCREMENT,
+  Nome_Funcionario VARCHAR(40) UNIQUE,
+  CPF_Funcionario VARCHAR(11) UNIQUE,
+  RG_Funcionario VARCHAR(9) UNIQUE,
   Sexo_Funcionario VARCHAR(9),
   Data_Nascimento_Funcionario DATE,
   Possui_Habilitacao_Funcionario BOOLEAN,
@@ -48,6 +47,7 @@ Quantidade_Horas_Trabalhadas INT(4),
 Data_Inicio_Trabalho DATE,
 Data_Fim_Trabalho DATE
 );
+
 
 CREATE TABLE Supervisao(
 ID_Supervisao INT(4) PRIMARY KEY,
