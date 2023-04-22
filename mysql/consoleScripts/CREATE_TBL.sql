832605dce060a8d774d565faec1f7686ec6644b4
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-04-21 23:18:52 -0300

    feat: Ajusre regra de negócio, -	Cada projeto possui um nome único em seu departamento;

--- mysql/consoleScripts/CREATE_TBL.sql
+++ mysql/consoleScripts/CREATE_TBL.sql
@@ -1,12 +1,50 @@
-###########################################################
--- Criar uma tabela por vez devido das chaves estrangeiras 
-###########################################################
+-- -	Cada departamento possui um nome único; 
+-- -	Um departamento possui diversos funcionários;
+-- Relacionamento (1:N)
+-- -	
+
 
 
 CREATE TABLE Departamento (
   ID_Departamento INT(4) AUTO_INCREMENT PRIMARY KEY,
   Nome_Departamento VARCHAR(40) UNIQUE NOT NULL
 );
+
+CREATE TABLE Projeto (
+  ID_Projeto INT(4) AUTO_INCREMENT PRIMARY KEY,
+  Nome_Projeto VARCHAR(40) NOT NULL,
+  Quantidade_Horas_Necessarias INT(4),
+  Prazo_Estimado INT(4),
+  Quantidade_Horas_Realizadas INT(4),
+  Data_Ultimo_Calculo_Horas DATE,
+  ID_Departamento INT(4),
+  Carga_Horaria_Supervisao INT(4),
+  ID_Funcionario_Supervisor INT(4),
+  FOREIGN KEY (ID_Funcionario_Supervisor) REFERENCES Funcionario(ID_Funcionario),
+  FOREIGN KEY (ID_Departamento) REFERENCES Departamento(ID_Departamento),
+  FOREIGN KEY (ID_Funcionario_Supervisor) REFERENCES Funcionario(ID_Funcionario),
+  CONSTRAINT UQ_Nome_Projeto_Departamento UNIQUE (ID_Departamento, Nome_Projeto)
+);
+
+
+-- teste insert projetos em departamentos diferentes k 
+INSERT INTO Projeto (Nome_Projeto, ID_Departamento) VALUES
+('SISTEMA ERP', 1),
+('Landing Page', 1),
+('SISTEMA ERP', 2),
+('API REST CRUD', 2);
+
+
+SELECT Nome_Projeto, ID_Departamento, ID_Projeto FROM  Projeto;
+
+-- teste erro mesmo nome no mesmo departamento 
+INSERT INTO Projeto (Nome_Projeto, ID_Departamento) VALUES
+('SISTEMA ERPERRO', 1),
+('SISTEMA ERPERRO', 1);
+
+-- fim do teste
+
+
 
 CREATE TABLE Funcionario (
   ID_Funcionario INT(4) AUTO_INCREMENT PRIMARY KEY,
@@ -23,21 +61,6 @@ CREATE TABLE Funcionario (
 );
 
 
-
-CREATE TABLE Projeto (
-  ID_Projeto INT(4) AUTO_INCREMENT PRIMARY KEY,
-  Nome_Projeto VARCHAR(40) NOT NULL,
-  Quantidade_Horas_Necessarias INT(4),
-  Prazo_Estimado INT(4),
-  Quantidade_Horas_Realizadas INT(4),
-  Data_Ultimo_Calculo_Horas DATE,
-  ID_Departamento INT(4),
-  Carga_Horaria_Supervisao INT(4),
-  ID_Funcionario_Supervisor INT(4),
-  FOREIGN KEY (ID_Funcionario_Supervisor) REFERENCES Funcionario(ID_Funcionario),
-  FOREIGN KEY (ID_Departamento) REFERENCES Departamento(ID_Departamento),
-  FOREIGN KEY (ID_Funcionario_Supervisor) REFERENCES Funcionario(ID_Funcionario)
-);
 
 
 
