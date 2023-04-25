b2a7edc0dd6597323cb84523ab7c8b7623918cf1
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-04-25 00:08:07 -0300

    fix: Ajsute para que a trigger atualiza_prazo_estimado funcione como esperado

--- mysql/consoleScripts/TRIGGER_atualiza_horas_realizadas.sql
+++ mysql/consoleScripts/TRIGGER_atualiza_horas_realizadas.sql
@@ -1,12 +1,11 @@
-	
 CREATE TRIGGER atualiza_horas_realizadas
-AFTER INSERT ON Funcionario_Projeto
+BEFORE INSERT ON Funcionario_Projeto
 FOR EACH ROW
 BEGIN
   DECLARE semanas_passadas DECIMAL(10,2);
   DECLARE horas_trabalhadas INT;
   
-  SELECT TIMESTAMPDIFF(WEEK, p.Data_Ultimo_Calculo_Horas, NOW()) INTO semanas_passadas
+  SELECT TIMESTAMPDIFF(WEEK, COALESCE(p.Data_Ultimo_Calculo_Horas, NOW()), NOW()) INTO semanas_passadas
   FROM Projeto p WHERE p.ID_Projeto = NEW.ID_Projeto;
   
   SELECT SUM(fp.Carga_Horaria) INTO horas_trabalhadas
@@ -14,11 +13,12 @@ BEGIN
   WHERE fp.ID_Projeto = NEW.ID_Projeto;
   
   UPDATE Projeto SET Data_Ultimo_Calculo_Horas = NOW() WHERE ID_Projeto = NEW.ID_Projeto;
- IF semanas_passadas IS NULL THEN
-  UPDATE Projeto SET Quantidade_Horas_Realizadas = Quantidade_Horas_Realizadas + horas_trabalhadas
-  WHERE ID_Projeto = NEW.ID_Projeto;
- ELSE
- UPDATE Projeto SET Quantidade_Horas_Realizadas = Quantidade_Horas_Realizadas + (horas_trabalhadas * semanas_passadas)
-  WHERE ID_Projeto = NEW.ID_Projeto;
- END IF;
+  
+  IF semanas_passadas <= 0 THEN
+    UPDATE Projeto SET Quantidade_Horas_Realizadas = Quantidade_Horas_Realizadas + horas_trabalhadas
+    WHERE ID_Projeto = NEW.ID_Projeto;
+  ELSE
+    UPDATE Projeto SET Quantidade_Horas_Realizadas = Quantidade_Horas_Realizadas + (horas_trabalhadas * semanas_passadas)
+    WHERE ID_Projeto = NEW.ID_Projeto;
+  END IF;
 END;
