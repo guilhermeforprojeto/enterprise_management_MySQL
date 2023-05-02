d200132df53ea2fa1f895cc747716063d1ceeb31
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-05-01 21:13:15 -0300

    feat:  atualizei o serializer  importando model Funcionario

--- django/GerenciamentoEmpresarial/empresa/api/serializers.py
+++ django/GerenciamentoEmpresarial/empresa/api/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from empresa import models
-       
+
 class DepartamentoSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = models.Departamento
-    fields = '__all__'
+    class Meta:
+        model = models.Departamento
+        fields = '__all__'
 
 
- 
+class FuncionarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Funcionario
+        fields = '__all__'
