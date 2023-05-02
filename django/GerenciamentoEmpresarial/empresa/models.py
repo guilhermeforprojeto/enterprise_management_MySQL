124316a7cff59a95447f091197873384c7753b6e
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-05-01 21:10:40 -0300

    feat: add models para criar tabelas departamento e funcionario

--- django/GerenciamentoEmpresarial/empresa/models.py
+++ django/GerenciamentoEmpresarial/empresa/models.py
@@ -2,17 +2,25 @@ from django.db import models
 
 import uuid
 
-
+# Criar table
 class Departamento(models.Model):
-    ID_Departamento = models.CharField(
-        primary_key=True, max_length=4, editable=False)
-    Nome_Departamento = models.CharField(max_length=40)
-
-    def save(self, *args, **kwargs):
-        if not self.ID_Departamento:
-            # Gera um novo UUID e guarda apenas os 4 primeiros caracteres
-            self.ID_Departamento = uuid.uuid4().hex[:4]
-        super().save(*args, **kwargs)
+    ID_Departamento = models.AutoField(primary_key=True)
+    Nome_Departamento = models.CharField(max_length=40, unique=True)
 
     class Meta:
         db_table = 'Departamento'
+
+class Funcionario(models.Model):
+    ID_Funcionario = models.AutoField(primary_key=True)
+    Nome_Funcionario = models.CharField(max_length=42, unique=True)
+    CPF_Funcionario = models.CharField(max_length=13, unique=True, null=True, blank=True)
+    RG_Funcionario = models.CharField(max_length=12, unique=True, null=True, blank=True)
+    Sexo_Funcionario = models.CharField(max_length=12, null=True, blank=True)
+    Data_Nascimento_Funcionario = models.DateField(null=True, blank=True)
+    Possui_Habilitacao_Funcionario = models.BooleanField(default=False)
+    Salario_Funcionario = models.CharField(max_length=12, null=True, blank=True)
+    Carga_Horaria_Semanal_Funcionario = models.CharField(max_length=12, null=True, blank=True)
+    ID_Departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Funcionario'
