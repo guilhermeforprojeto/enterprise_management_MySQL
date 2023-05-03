ee0a7a96971e3daaf6c23e5be4f1f2fd1dd65379
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-05-02 23:32:25 -0300

    feat: Rota para Projeto

--- django/GerenciamentoEmpresarial/GerenciamentoEmpresarial/urls.py
+++ django/GerenciamentoEmpresarial/GerenciamentoEmpresarial/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-from empresa.api.viewsets import DepartamentoViewSet, FuncionarioViewSet
+from empresa.api.viewsets import DepartamentoViewSet, FuncionarioViewSet, ProjetoViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'departamento', DepartamentoViewSet, basename='departamento')
 router.register(r'funcionario', FuncionarioViewSet, basename='funcionario')
+router.register(r'projeto', ProjetoViewSet, basename='projeto')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
