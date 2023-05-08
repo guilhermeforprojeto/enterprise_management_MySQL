764b4c94cc659e17150e61b4e7cc2159ccc8658b
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-05-08 20:02:38 -0300

    feat: primeiros ajustes para usar  a procedure 001proc_departamento_crud

--- django/GerenciamentoEmpresarial/GerenciamentoEmpresarial/urls.py
+++ django/GerenciamentoEmpresarial/GerenciamentoEmpresarial/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework import routers
-from empresa.api.viewsets import DepartamentoViewSet, FuncionarioViewSet, ProjetoViewSet
+from empresa.api.viewsets import DepartamentoProcedureViewSet
 
 router = routers.DefaultRouter()
-
-router.register(r'departamento', DepartamentoViewSet, basename='departamento')
-router.register(r'funcionario', FuncionarioViewSet, basename='funcionario')
-router.register(r'projeto', ProjetoViewSet, basename='projeto')
+router.register(r'departamento_procedure', DepartamentoProcedureViewSet, basename='departamento_procedure')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- django/GerenciamentoEmpresarial/empresa/api/viewsets.py
+++ django/GerenciamentoEmpresarial/empresa/api/viewsets.py
@@ -1,77 +1,53 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.response import Response
-from empresa.api import serializers
-from empresa import models
-from .serializers import FuncionarioSerializer
+from django.db import connection
 
-
-class DepartamentoViewSet(viewsets.ViewSet):
-    serializer_class = serializers.DepartamentoSerializer
-
-    def list(self, request):
-        departamentos = models.Departamento.objects.all()
-        serializer = self.serializer_class(departamentos, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        departamento = models.Departamento.objects.get(pk=pk)
-        serializer = self.serializer_class(departamento)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        departamento = models.Departamento.objects.get(pk=pk)
-        serializer = self.serializer_class(departamento, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        departamento = models.Departamento.objects.get(pk=pk)
-        departamento.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class FuncionarioViewSet(viewsets.ViewSet):
-    serializer_class = serializers.FuncionarioSerializer 
-    def list(self, request):
-        queryset = models.Funcionario.objects.all() # alteração aqui
-        serializer = FuncionarioSerializer(queryset, many=True)
-        return Response(serializer.data)
+class DepartamentoProcedureViewSet(viewsets.ViewSet):
     
     def create(self, request):
-        serializer = FuncionarioSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def retrieve(self, request, pk=None):
-        queryset = models.Funcionario.objects.all() # alteração aqui
-        funcionario = get_object_or_404(queryset, pk=pk)
-        serializer = FuncionarioSerializer(funcionario)
-        return Response(serializer.data)
-    
+        json_str = request.data.get('json_str')
+        op = request.data.get('op')
+
+        with connection.cursor() as cursor:
+            cursor.callproc('001proc_departamento_crud', [json_str, op])
+            result = cursor.fetchall()
+
+        # Realize qualquer tratamento necessário nos resultados da procedure
+        # e retorne a resposta apropriada
+        return Response({'message': 'Operação executada com sucesso!'})
+
     def update(self, request, pk=None):
-        funcionario = models.Funcionario.objects.get(pk=pk) # alteração aqui
-        serializer = FuncionarioSerializer(funcionario, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def destroy(self, request, pk=None):
-        funcionario = models.Funcionario.objects.get(pk=pk) # alteração aqui
-        funcionario.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        json_str = request.data.get('json_str')
+        op = request.data.get('op')
 
+        with connection.cursor() as cursor:
+            cursor.callproc('001proc_departamento_crud', [json_str, op])
+            result = cursor.fetchall()
 
-class ProjetoViewSet(viewsets.ModelViewSet):
-    queryset = models.Projeto.objects.all()
-    serializer_class = serializers.ProjetoSerializer
+        # Realize qualquer tratamento necessário nos resultados da procedure
+        # e retorne a resposta apropriada
+        return Response({'message': 'Operação executada com sucesso!'})
+
+    def delete(self, request, pk=None):
+        json_str = request.data.get('json_str')
+        op = request.data.get('op')
+
+        with connection.cursor() as cursor:
+            cursor.callproc('001proc_departamento_crud', [json_str, op])
+            result = cursor.fetchall()
+
+        # Realize qualquer tratamento necessário nos resultados da procedure
+        # e retorne a resposta apropriada
+        return Response({'message': 'Operação executada com sucesso!'})
+
+    def retrieve(self, request, pk=None):
+        json_str = request.data.get('json_str')
+        op = request.data.get('op')
+
+        with connection.cursor() as cursor:
+            cursor.callproc('001proc_departamento_crud', [json_str, op])
+            result = cursor.fetchall()
+
+        # Realize qualquer tratamento necessário nos resultados da procedure
+        # e retorne a resposta apropriada
+        return Response({'message': 'Operação executada com sucesso!'})
